2c7c6257262fb07e65b2c9251c41d9a3597d2493
Andrey Krivtsov <andrey.krivtsov@idp.zyfra.com>
2023-11-15 10:12:26 +0000

    mode dev container spec to separate repo

--- .devcontainer/features/azure-cli-completion/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Installs Azure CLI bash autocompletion"
-curl https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion --output /etc/bash_completion.d/az.completion
-
