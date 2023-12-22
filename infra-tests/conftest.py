204e1b2eb2775e8b9d938a264e4161828008bfe5
Andrey Krivtsov <andrey.krivtsov@idp.zyfra.com>
2023-12-22 20:40:52 +0000

    add tests

--- infra-tests/conftest.py
+++ infra-tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 
 def pytest_addoption(parser: pytest.Parser):
-    parser.addoption("--front-url", dest="front_url", type=str, default="http://localhost:8000")
-    parser.addoption("--backdb-url", dest="backdb_url", type=str, default="http://localhost:8100")
-    parser.addoption("--backstor-url", dest="backstor_url", type=str, default="http://localhost:8200")
+    parser.addoption("--app-urls", dest="app_urls", type=str, default="http://localhost:8000,http://localhost:8100,http://localhost:8200")
     parser.addoption("--commit", dest="commit", type=str, default="demo-commit-sha")
 
 
 
+def pytest_generate_tests(metafunc: pytest.Metafunc):
+    if "app_url" in metafunc.fixturenames:
+        values = [ s.strip() for s in str(metafunc.config.getoption("app_urls")).split(',') ]
+        metafunc.parametrize("app_url", values)
--- infra-tests/test_apps.py
+++ infra-tests/test_apps.py
@@ -7,39 +7,16 @@ http = urllib3.PoolManager(retries=retries)
 
 HEALTH_PATH = "/health/live"
 
-class AppEnum(str, Enum):
-    front = "front"
-    backdb = "backdb"
-    backstor = "backstor"
-
-@pytest.fixture
-def app_urls(pytestconfig: pytest.Config) -> dict:
-    return {
-        AppEnum.front: pytestconfig.getoption("front_url"),
-        AppEnum.backdb: pytestconfig.getoption("backdb_url"),
-        AppEnum.backstor: pytestconfig.getoption("backstor_url"),
-    }
-
 @pytest.fixture
 def git_commit(pytestconfig: pytest.Config) -> str:
     return pytestconfig.getoption("commit")
 
 class TestApplications:
-    def _test_app(self, url: str, git_commit :str):
-        resp = http.request("GET", url + HEALTH_PATH)
+    def test_app(self, app_url: str, git_commit :str):
+        resp = http.request("GET", app_url + HEALTH_PATH)
 
         assert resp.status == 200
 
         data = resp.json()
         assert data["commit"] == git_commit
-
-
-    def test_front(self, app_urls: dict, git_commit: str):
-        self._test_app(app_urls[AppEnum.front], git_commit)
-
-    def test_backdb(self, app_urls: dict, git_commit: str):
-        self._test_app(app_urls[AppEnum.backdb], git_commit)
-
-    def test_backstor(self, app_urls: dict, git_commit: str):
-        self._test_app(app_urls[AppEnum.backstor], git_commit)
 
